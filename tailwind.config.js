9184d1d1beccdd24bf87d196695eed96894fe715
fiamanillah <fiamanillahb2@gmail.com>
2024-08-24 01:55:02 +0600

    Hero and about section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
 
-    screens: {
-      '2xl': {'max': '1535px'},
-      // => @media (max-width: 1535px) { ... }
-
-      'xl': {'max': '1279px'},
-      // => @media (max-width: 1279px) { ... }
-
-      'lg': {'max': '1023px'},
-      // => @media (max-width: 1023px) { ... }
-
-      'md': {'max': '767px'},
-      // => @media (max-width: 767px) { ... }
-
-      'sm': {'max': '639px'},
-      // => @media (max-width: 639px) { ... }
-    },
+    
 
 
 
@@ -27,15 +12,34 @@ module.exports = {
         "primary": "#ABEF5F",
         "primary-50": "#ABEF5F80",
         "primary-20": "#ABEF5F30",
-        "secondary-bg": "#f4f4f4",
+        "primary-bg": "#f8f8f8",
+        "secondary-bg": "#ebebeb",
         "text-primary": "#212121",
         "text-secondary": "#494949",
         "text-secondary-2": "#737373",
       },
       fontFamily: { 
         "manrope": ['Manrope', 'sans-serif'] 
-    } 
     },
+
+    screens: {
+      '2xl-d': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl-l': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg-t': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md-p': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm-p': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
+    },
+
   },
   plugins: [],
 }
